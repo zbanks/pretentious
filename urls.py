cabe8ed94d51ae79d80e002a270cc62d98157bef
Zach Banks <zbanks@mit.edu>
2012-01-12 07:09:49 -0500

    Static

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,4 +17,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'pretentious.pos.views.index', name='index'),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
