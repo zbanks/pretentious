a8c90b50eb2d0864a2ad0bd0c1c072925ed1095c
Zach Banks <zbanks@mit.edu>
2012-01-17 20:09:01 -0500

    Fixed javascript

--- pos/static/js/interface.js
+++ pos/static/js/interface.js
@@ -4,32 +4,26 @@ $(document).ready(function(){
     for(var i = 1; i <= 9; i++){
         $("div.box"+i).data("index", i).click(function(){
             $this = $(this);
-            console.log($this.data("index"));
             PRODUCTS[$this.data("index")] = $this.attr("productslug");
             setOrderbar($this.data("index"));
         });
     }
     $("div.box0").data("index", 0).click(function(){
         $this = $(this);
-        console.log($this.data("index"));
         setOrderbar("credit");
     }); 
 
     $("li.customer").each(function(i){
         $this = $(this);
-        $this.username = $this.html();
+        var username = $this.html();
         $this.click(function(){
-            console.log($this.username);
-            setOrderbar(null, $this.username);
-
-            //submitOrder();
+            setOrderbar(null, username);
         });
     });
 
     parseOrderbar = function(){
-        parseExpr = /\s*([0-9]{1,2})\s+([a-zA-Z0-9_-]+)\s*/i
+        parseExpr = /\s*([0-9]+)\s+([a-zA-Z0-9_-]+)?\s*/i
         var ps = $("input.orderbar").val().match(parseExpr);
-        console.log(ps);
         if(!ps){
             return [0, ""];
         }else if(ps.length > 2){
@@ -41,6 +35,7 @@ $(document).ready(function(){
 
     setOrderbar = function(number, username){
         var state = parseOrderbar();
+        console.log(state);
         number = number || state[0];
         username = username || state[1];
 
